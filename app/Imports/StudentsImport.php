19bb85f1181c5975aff2ebe72e9897df5009ed06
Amirreza Shahidi <shahidiam83@gmail.com>
2024-01-30 03:50:46 +0330

    add sheet events

--- app/Imports/StudentsImport.php
+++ app/Imports/StudentsImport.php
@@ -10,6 +10,9 @@ use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 use Hash;
 use Filament\Notifications\Notification;
+use Illuminate\Support\Str;
+use Maatwebsite\Excel\Events\BeforeSheet;
+use Maatwebsite\Excel\Concerns\WithEvents;
 
 class StudentsImport implements OnEachRow,WithStartRow, WithEvents
 {
@@ -18,9 +21,10 @@ class StudentsImport implements OnEachRow,WithStartRow, WithEvents
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
-    private $sheetNames;
+    private $currentCode;
+    
     function __construct(private $options){
-	$this->sheetNames = [];
+	    $this->currentCode = 1;
     }
 
     public function onRow(Row $row)
@@ -31,17 +35,22 @@ class StudentsImport implements OnEachRow,WithStartRow, WithEvents
 		$row[$index] = trim($row[$index]);
 	}
 
-        $grades = ['هفتم' => 1,'هشتم' => 2,'نهم' => 3,'دهم' => 4,'یازدهم' => 5,'دوازدهم' => 6,'فارغ' => 6];
-        $majors = ['' => 0,'بدون رشته' => 0,'ریاضی' => 1,'تجربی' => 2,'انسانی' => 3];
+    $grades = ['هفتم' => 1,'هشتم' => 2,'نهم' => 3,'دهم' => 4,'یازدهم' => 5,'دوازدهم' => 6,'فارغ' => 6];
+    $majors = ['' => 0,'بدون رشته' => 0,'ریاضی' => 1,'تجربی' => 2,'انسانی' => 3];
 
 	if(strlen($row[1]) == 0)
 		return;
         $is_name_seperated = $this->options['seperated_name'] ?? false;
+        $phoneNumber = $is_name_seperated ? $row[3] : $row[2];
+        
+        if(User::where('phoneNumber',$phoneNumber)->count() > 0)
+            return;
+
         $user = User::firstOrCreate(
             [
                 'password' => Hash::make('12345678'),
                 'name' =>  $is_name_seperated ? $row[1] .' '. $row[2] : $row[1],
-                'phoneNumber' => $is_name_seperated ? $row[3] : $row[2],
+                'phoneNumber' => $phoneNumber,
                 'role' => 2,
                 'status' => $this->options['status'],
                 'rate' => 0,
@@ -55,7 +64,7 @@ class StudentsImport implements OnEachRow,WithStartRow, WithEvents
                 'user_id' => $user->id,
                 'grade' => array_key_exists($grade_row,$grades ) ? $grades[$grade_row] : 6,
                 'major' => array_key_exists($major_row,$majors) ? $majors[$major_row] : 0,
-                'counselor_id' => isset($this->options['counselor']) ? $this->options['counselor']->id : null,
+                'counselor_id' => $this->currentCode,
                 'school' => $this->options['school'],
                 'goal' => 'هدف شما',
                 'rate' => 0
@@ -74,10 +83,30 @@ class StudentsImport implements OnEachRow,WithStartRow, WithEvents
    }
     public function registerEvents(): array
     {
-	return [
-					            BeforeSheet::class => function(BeforeSheet $event) {
-							                    $this->sheetNames[] = $event->getSheet()->getTitle();
-									                } 
-				        ];
-				    }
+	    return [
+		    BeforeSheet::class => function(BeforeSheet $event) {
+				$sheetName = $event->getSheet()->getTitle();
+                $random = Str::random(8);
+                $user = User::create(
+                    [
+                        'phoneNumber' => $random,
+                        'name' => $sheetName,
+                        'role' => 1,
+                        'password' => Hash::make('12345678'),
+                        'status' => 1,
+                        'rate' => 0
+                    ]
+                );
+
+                $counselor = Counselor::create(
+                    [
+                        'code' => $random,
+                        'user_id' => $user->id
+                    ]
+                );
+
+                $this->currentCode = $counselor->id;
+			} 
+		];
+	}
 }
